1f93d940e16b029e65ad86e441b3b0366ed2a0a9
unknown <kelvtmoni@gmail.com>
2025-04-24 10:12:36 +0100

    added sign in and sign out functionality in the header component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HomePage from "./pages/homepage/homepage.component";
 import SignInAndSignUpPage from "./pages/signin-and-signup-page/signin-and-signup.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import { auth } from "./firebase/firebase.utils";
+import React from "react";
 
 const HatsPage = () => (
   <div>
@@ -11,18 +13,39 @@ const HatsPage = () => (
   </div>
 );
 
-function App() {
-  return (
-    <div>
-      <Header />
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+  unSubscribeFromAuth = null;
 
-      <Routes>
-        <Route exact path="/" Component={HomePage}></Route>
-        <Route path="/shop" Component={ShopPage}></Route>
-        <Route path="/signIn" Component={SignInAndSignUpPage}></Route>
-      </Routes>
-    </div>
-  );
+  componentDidMount() {
+    this.unSubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unSubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+
+        <Routes>
+          <Route exact path="/" Component={HomePage}></Route>
+          <Route path="/shop" Component={ShopPage}></Route>
+          <Route path="/signIn" Component={SignInAndSignUpPage}></Route>
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,19 +2,29 @@ import React from "react";
 import "./header.style.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { Link } from "react-router-dom";
+import { auth } from "../../firebase/firebase.utils";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
-      <Logo className="logo"></Logo>
+      <Logo className="logo" title="Crown Logo"></Logo>
     </Link>
     <div className="options">
       <Link className="option" to="/shop">
         SHOP
       </Link>
-      <Link className="option" to="/shop">
+      <Link className="option" to="/contact">
         CONTACT
       </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGNOUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGNIN
+        </Link>
+      )}
     </div>
   </div>
 );
